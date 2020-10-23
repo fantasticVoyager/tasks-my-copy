26cee18b4d50224c348740fffdd9c80a13bca883
Keith Rasweiler <saintmarzano@gmail.com>
2020-10-22 22:35:36 -0400

    Implementation of the list component with fake data.

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+    tasks: Task[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+      this.tasks.push(new Task(1, "Task 1", false, "5/7/2020"));
+      this.tasks.push(new Task(2, "Task 2", false, "5/7/2020"));
+      this.tasks.push(new Task(3, "Task 3", true, "5/7/2020"));
   }
+
+    getDueDateLabel(task: Task) {
+      return task.completed ? 'label-success' : 'label-primary';
+    }
+
+    onTaskChange(event, task: Task) {
+      console.log("task has changed");
+      // this.taskService.saveTask(task, event.target.checked).subscribe();
+    }
 
 }
